2bc86ce609a7d4cb70d39aa38f60805f03c594a6
supermeisi <mustafa_schmidt@gmx.de>
2026-09-18 10:03:05 +0000

    Adding download of ISIN numbers

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import csv
+import urllib.request
 
+
+def get_isin_list(url):
+    response = urllib.request.urlopen(url)
+    lines = [line.decode("utf-8") for line in response.readlines()]
+    reader = csv.reader(lines, delimiter=";")
+
+    print('Printing first 5 ISIN:')
+
+    for row in list(reader)[3:8]:
+        print(row[3])
+
+
+def main():
+    url = "https://www.cashmarket.deutsche-boerse.com/resource/blob/1528/5d846a1a320a80f824bcd1b9db5f3067/data/t7-xetr-allTradableInstruments.csv"
+    get_isin_list(url)
+
+
+if __name__ == "__main__":
+    main()
